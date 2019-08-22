24cf1ca991ef902715cd793fb49c7ca57a1fe682
lanten <974986907@qq.com>
2019-08-22 14:50:39 +0800

    update

--- build/build.js
+++ build/build.js
@@ -11,17 +11,22 @@ const { BUILD_ENV, NODE_ENV } = process.env
 
 console.log(NODE_ENV, BUILD_ENV)
 
-const envList = Object.keys(devConfig.env)
-prompt({
-  type: 'list',
-  name: 'env',
-  message: '\n\n选择编译到哪个环境?',
-  choices: envList,
-  default: envList[0],
-}).then(res => buildDist(res.env))
+if (BUILD_ENV) {
+  buildDist(BUILD_ENV)
+} else {
+  const envList = Object.keys(devConfig.env)
+  prompt({
+    type: 'list',
+    name: 'env',
+    message: '\n\n选择编译到哪个环境?',
+    choices: envList,
+    default: envList[0],
+  }).then(res => buildDist(res.env))
+}
 
 function buildDist(env) {
-  if (env != 'prod') { // 测试环境启用 vConsole
+  if (env != 'prod') {
+    // 测试环境启用 vConsole
     webpackConfig.plugins.push(new vConsolePlugin({ enable: true }))
     console.log('=> VConsole 已启用')
   }
